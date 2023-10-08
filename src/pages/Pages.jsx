9f7bd84fd04edc499a97fcd2919a53719a2540ee
maponbie <martha.aponbiede@valpo.edu>
2023-10-07 21:42:20 -0500

    Recipe Commit

---
+++ src/pages/Pages.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Home from "./Home";
+import Cuisine from "./Cuisine";
+import Searched from "./Searched"; // Corrected the import path
+import Recipe from "./Recipe";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+
+function Pages() {
+  const location = useLocation();
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <Routes location={location} key={location.pathname}> {/* Corrected the 'Location' prop */}
+        <Route path="/" element={<Home />} />
+        <Route path="/cuisine/:type" element={<Cuisine />} />
+        <Route path="/searched/:search" element={<Searched />} />
+        <Route path="/recipe/:name" element={<Recipe />} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
+export default Pages;
